4e2276ba241e8162577f5b847192a59c3dcc38d6
KANG JIHYO <luxuryjihyo@naver.com>
2024-11-22 19:40:57 +0900

    tkinter

---
+++ gui_basic/13_scrollbar.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()
+root.title("Nado GUI")
+root.geometry("640x480+300+100") #가로 * 새로 + x좌표 + y좌표
+
+frame = Frame(root)
+frame.pack()
+
+scrollbar = Scrollbar(frame)
+scrollbar.pack(side="right", fill="y")
+
+listbox = Listbox(frame, selectmode="extended", height=10, yscrollcommand=scrollbar.set)
+for i in range(1,32):
+    listbox.insert(END, str(i) + "일")
+listbox.pack(side="left")
+
+scrollbar.config(command=listbox.yview)
+
+root.mainloop()
